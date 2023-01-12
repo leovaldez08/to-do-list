b9a80b4057fa55b5b3b1600b907bd3ae3900c075
Rishi Ganesh <rishi.ganesh@kalvium.community>
2023-01-12 13:28:27 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -7,6 +7,34 @@ var toDoList = document.getElementById('todolist');
 document.getElementById('button').onclick = () => {
   listOfActivities.push(input.value);
   console.log(listOfActivities);
-};
+  input.value = ""
+
+  showList();
+}
+
+function showList() {
+  toDoList.innerHTML = "";
+
+  listOfActivities.forEach(function(n, i){
+    toDoList.innerHTML += "<li>" + n + 
+    "<a onclick='edititem(" + i +
+    ")'>Edit</a>" +
+    "<a onclick='deleteitem(" + i +
+    ")'>&times | <a/></li>";
+  });
+}
+
+function deleteitem(i) {
+  listOfActivities.splice(i, 1);
+
+  showList();
+}
+
+function edititem(i) {
+  var newvalue = prompt("Please insert your new value");
+  listOfActivities.splice(i, 1, newvalue);
+
+  showList();
+}
 
 
